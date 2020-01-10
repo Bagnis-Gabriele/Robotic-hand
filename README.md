85ef123e70e1105f6c68c6138c481576f12bca06
Gabriele Bagnis <gabriele.bagnis@itiscuneo.eu>
2020-01-10 21:56:08 +0100

    Reorganization of the repository and loading of the code to make python communicate with arduino

---
+++ README.md
@@ -0,0 +1,10 @@
+# Robotic-hand
+ A robotic hand based on motor diabilities. It helps people to regain joint mobility.
+ 
+ # Eletronics parts:
+ 1. Arduino UNO
+      - Servo mothors (x5) 
+ 2. Raspberry PI
+ 3. Emotiv EPOC+ EEG
+ 4. Leap Motion
+ 5. 3D Printed hand model
---
+++ code/Sending_Random_Servo_data_to_Arduino_from_python/Sending_Random_Servo_data_to_Arduino_from_python.py
@@ -0,0 +1,32 @@
+import sys, time, math, serial
+from pynput.keyboard import Key, Listener
+from random import randrange
+
+ARDUINO_PORT_NAME='COM7'
+FREQ_COMMANDS = 0.1
+arduino = serial.Serial(ARDUINO_PORT_NAME ,9600)
+timeCommand=0.0
+
+def on_press(key):
+    global timeCommand
+    if time.clock() - timeCommand > FREQ_COMMANDS:
+        stringa= str(randrange(2)) + str(randrange(8)) + str(randrange(10)) + str(randrange(2)) + str(randrange(8)) + str(randrange(10)) + str(randrange(2)) + str(randrange(8)) + str(randrange(10)) + str(randrange(2)) + str(randrange(8)) + str(randrange(10)) + str(randrange(2)) + str(randrange(8)) + str(randrange(10))
+        arduino.write(stringa)
+        print "Arduino:" + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read() + arduino.read()
+        print "numero 1:" + arduino.read() + arduino.read() + arduino.read()
+        print "numero 2:" + arduino.read() + arduino.read() + arduino.read()
+        print "numero 3:" + arduino.read() + arduino.read() + arduino.read()
+        print "numero 4:" + arduino.read() + arduino.read() + arduino.read()
+        print "numero 5:" + arduino.read() + arduino.read() + arduino.read()
+        timeCommand = time.clock()
+
+def on_release(key):
+    if key == Key.esc:
+        # Stop listener
+        return False
+
+with Listener(
+    on_press=on_press,
+    on_release=on_release) as listener:
+    listener.join()
+
