9bae9d46b95dff9a974ad26b772e73e357390ccd
Gabriele Bagnis <gabriele.bagnis@itiscuneo.eu>
2020-01-14 12:31:58 +0100

    edit python code

---
+++ definitive_code/Emotiv/config.py
@@ -0,0 +1,10 @@
+#Parameters to be changed
+ARDUINO_PORT_NAME = 'COM9'
+
+#Fixed parameters
+ARDUINO_SERIAL = 9600
+FREQ_COMMANDS = 0.05
+OPEN = "000000000000000"
+CLOSE = "180180180180180"
+K_O = "u'o'"
+K_C = "u'c'"
